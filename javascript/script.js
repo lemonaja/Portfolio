210fcaf31b85f80c033ac06fe0eadba98d6493c9
Fernando <febibilemos@gmail.com>
2024-06-29 01:06:57 -0300

    Arrumei quase tudo que tinha para arrumar, só falta mudar o projeto do behance

--- javascript/script.js
+++ javascript/script.js
@@ -1,47 +1,47 @@
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
+  constructor(mobileMenu, navList, navLinks) {
       this.mobileMenu = document.querySelector(mobileMenu);
       this.navList = document.querySelector(navList);
       this.navLinks = document.querySelectorAll(navLinks);
       this.activeClass = "active";
-  
+
       this.handleClick = this.handleClick.bind(this);
-    }
-  
-    animateLinks() {
+  }
+
+  animateLinks() {
       this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-              index / 7 + 0.3
-            }s`);
+          link.style.animation
+              ? (link.style.animation = "")
+              : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
       });
-    }
-  
-    handleClick() {
+  }
+
+  handleClick() {
       this.navList.classList.toggle(this.activeClass);
       this.mobileMenu.classList.toggle(this.activeClass);
       this.animateLinks();
-    }
-  
-    addClickEvent() {
+  }
+
+  addClickEvent() {
       this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
+  }
+
+  init() {
       if (this.mobileMenu) {
-        this.addClickEvent();
+          this.addClickEvent();
       }
       return this;
-    }
   }
-  
+}
+
 const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-  );
-  mobileNavbar.init();
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
+
+
 
 function typeWriter(elemento) {
     const textoArray = elemento.innerHTML.split('');
